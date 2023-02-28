3804b900c0c31af219aa7c28ae331c1f92f33e28
DESKTOP-C22IVHJ\TrongHuy_Mac <tronghuy2208@gmail.com>
2023-02-28 16:54:35 +0700

    cap nhat

---
+++ src/GoZone.BackendServer/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoZone.BackendServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- src/GoZone.BackendServer/Program.cs
+++ src/GoZone.BackendServer/Program.cs
@@ -1,14 +1,12 @@
 using FluentValidation.AspNetCore;
 using GoZone.BackendServer.Data;
 using GoZone.BackendServer.Data.Entities;
+using GoZone.BackendServer.IdentityServer;
 using GoZone.ViewModels.Systems;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using Serilog;
-using System;
 
 Log.Logger = new LoggerConfiguration()
                             .Enrich.FromLogContext()
@@ -21,14 +19,21 @@ builder.Host.UseSerilog();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
-
+builder.Services.AddRazorPages();
 //2. Setup idetntity
 builder.Services.AddIdentity<AppUser, IdentityRole>()
                .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddIdentityServer(options =>
 {
     options.Events.RaiseErrorEvents = true;
-});
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+})
+.AddInMemoryApiResources(Config.Apis)
+.AddInMemoryClients(Config.Clients)
+.AddInMemoryIdentityResources(Config.Ids)
+.AddAspNetIdentity<AppUser>();
 builder.Services.Configure<IdentityOptions>(options =>
 {
     // Password settings
@@ -61,6 +66,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapRazorPages();
     app.UseSwagger();
     app.UseSwaggerUI(m =>
     {
@@ -68,10 +74,19 @@ if (app.Environment.IsDevelopment())
     });
 }
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseIdentityServer();
 app.UseAuthorization();
-app.MapControllers();
-//app.UseRouting();
-//app.MapRazorPages();
+//app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "areaRoute",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
